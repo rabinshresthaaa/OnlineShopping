da0f0a4979f19d1a03c5fcae537f792671500e54
Rabin Shrestha <121755408+itsrawbin@users.noreply.github.com>
2025-12-03 22:14:33 +0545

    Created Category Model

---
+++ Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecommerce.Models
+{
+    public class Category
+    {
+        public int Id { get; set; };
+        public string Name { get; set; };
+        public string? Slug { get; set; };
+        
+    }
+}
