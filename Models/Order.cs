67354c0305a9eb014e053835ecd2e67b421b59f7
Rabin Shrestha <121755408+itsrawbin@users.noreply.github.com>
2025-12-03 22:27:37 +0545

    Created Order Model

---
+++ Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace Ecommerce.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = null!;
+        public DateTime OrderTime { get; set; } = DateTime.UtcNow;
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; } = "Pending";
+        public ICollection<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public string ShippingAddress { get; set; } = null!;
+    }
+}
