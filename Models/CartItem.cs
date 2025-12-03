28db62006cce5957b048f61f677ed6fd3a362602
Rabin Shrestha <121755408+itsrawbin@users.noreply.github.com>
2025-12-03 22:21:50 +0545

    Created CartItem Model

---
+++ Models/CartItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Ecommerce.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public string? UserId { get; set; }
+        public string? SessionId { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = null!;
+        public int Quantity { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    }
+}
