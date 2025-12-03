9a314b35a403a4456c10f7215bdbc2d114c7202c
Rabin Shrestha <121755408+itsrawbin@users.noreply.github.com>
2025-12-03 22:18:38 +0545

    Created Product Model

--- Models/Category.cs
+++ Models/Category.cs
@@ -2,9 +2,9 @@
 {
     public class Category
     {
-        public int Id { get; set; };
-        public string Name { get; set; };
-        public string? Slug { get; set; };
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Slug { get; set; }
         
     }
 }
---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Ecommerce.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+        public int Stock {  get; set; }
+        public string? MainImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+        public string? SellerId { get; set; }
+
+    }
+}
